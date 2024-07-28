a5576fb7fa675c8a8b581589b3a1f36427c12a2d
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-07-28 18:01:52 -0400

    Add query selector module

---
+++ src/modules/dom-query.js
@@ -0,0 +1,45 @@
+let sidebarContainer = document.querySelector('.sidebar-container');
+let nav = document.querySelector('nav');
+let profileContainer = document.querySelector('.profile-container');
+let profileIcon = document.querySelector('#profile-icon');
+let userNameDisplay = document.querySelector('.user-name');
+let arrowIcon = document.querySelector('#arrow-icon');
+let notificationIcon = document.querySelector('#notification-icon');
+let collapseSidebarIcon = document.querySelector('#collapse-sidebar-icon');
+let addTaskContainer = document.querySelector('.add-task-container');
+let plusIcon = document.querySelector('.plus-icon');
+let addTaskButton = document.querySelector('.add-task');
+let taskFoldersContainer = document.querySelector('.task-folders');
+let inboxFolder = document.querySelector('.inbox');
+let todayFolder = document.querySelector('.today');
+let upcomingFolder = document.querySelector('.upcoming');
+let priorityFolder = document.querySelector('.priority');
+let completedFolder = document.querySelector('.completed');
+let projectFoldersContainer = document.querySelector('.project-folders-container');
+let myProjectsText = document.querySelector('.my-projects');
+let themeSelectorContainer = document.querySelector('.theme-selector-container');
+let contentContainer = document.querySelector('.content-container');
+
+export {
+    sidebarContainer,
+    nav,
+    profileContainer,
+    profileIcon,
+    userNameDisplay,
+    arrowIcon,
+    notificationIcon,
+    collapseSidebarIcon,
+    addTaskContainer,
+    plusIcon,
+    addTaskButton,
+    taskFoldersContainer,
+    inboxFolder,
+    todayFolder,
+    upcomingFolder,
+    priorityFolder,
+    completedFolder,
+    projectFoldersContainer,
+    myProjectsText,
+    themeSelectorContainer,
+    contentContainer
+}
