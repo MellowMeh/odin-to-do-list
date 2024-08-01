207bf96587914844cfdab67e322569a43d62a4b3
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-01 13:27:10 -0400

    Add detectLocalStorage module
    Function detects if the browser supports local storage

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,7 @@
 import '../style/index.css';
 import { toggleDarkMode, changeDisplayedTheme } from '../modules/theme-toggle';
+import { storageAvailable } from '../modules/detectLocalStorage';
 
 toggleDarkMode();
 changeDisplayedTheme();
+storageAvailable();
