0409ec12b7e2becc8adcca646520479f053c5d4d
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-02 14:27:48 -0400

    Cut unused imports

--- src/script/index.js
+++ src/script/index.js
@@ -1,10 +1,6 @@
 import '../style/index.css';
 import { toggleDarkMode, changeDisplayedTheme } from '../modules/theme-toggle';
 import { storageAvailable } from '../modules/detect-local-storage';
-import {getAllTasks} from '../modules/get-all-tasks';
-import { updateTasks } from '../modules/update-tasks';
-import { deleteTask } from '../modules/delete-task';
-import { taskCreator } from '../modules/create-task';
 import { addTaskContainer } from '../modules/dom-query';
 import { generateAddTaskPopUp } from '../modules/api-add-task';
 
