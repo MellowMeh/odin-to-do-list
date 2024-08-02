ec91d6170f80f7fea1bda56e058a8abcd055914a
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-02 14:28:58 -0400

    Fix add-task-pop-up module name

--- src/script/index.js
+++ src/script/index.js
@@ -2,7 +2,7 @@ import '../style/index.css';
 import { toggleDarkMode, changeDisplayedTheme } from '../modules/theme-toggle';
 import { storageAvailable } from '../modules/detect-local-storage';
 import { addTaskContainer } from '../modules/dom-query';
-import { generateAddTaskPopUp } from '../modules/api-add-task';
+import { generateAddTaskPopUp } from '../modules/add-task-pop-up';
 
 
 toggleDarkMode();
