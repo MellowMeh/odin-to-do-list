bb98881ebdf45b835706d684efc3b4120c85745e
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-04 16:57:40 -0400

    Fix theme toggle bug
    Revert to prior version. Query selector returned to #checkbox.

--- src/script/index.js
+++ src/script/index.js
@@ -1,11 +1,7 @@
 import '../style/index.css';
 import { toggleDarkMode } from '../modules/theme-toggle';
 import { storageAvailable } from '../modules/detect-local-storage';
-<<<<<<< HEAD
 import { themeSlider, addTaskContainer, collapseSidebarIcon, mainAddTaskContainer, navAddProjectContainer } from '../modules/dom-query';
-=======
-import { addTaskContainer, collapseSidebarIcon, mainAddTaskContainer } from '../modules/dom-query';
->>>>>>> parent of b2b4c19 (Add project modules)
 import { generateAddTaskPopUp } from '../modules/add-task-pop-up';
 import { generateInbox } from '../modules/inbox';
 import { collapseSidebar, generateExpandSidebarIcon } from '../modules/side-bar';
