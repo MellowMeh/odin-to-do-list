9e75fb685ad902edb941b43413c1f6a1e4673faa
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-04 13:55:37 -0400

    Add expand side bar to icon

--- src/modules/side-bar.js
+++ src/modules/side-bar.js
@@ -7,15 +7,19 @@ let collapseSidebar = () => {
     body.style.gridTemplateColumns = '0px 1fr';
 };
 
+let sidebarIcon;
+
 let generateExpandSidebarIcon = () => {
     let sidebarIconCode = '<svg xmlns="http://www.w3.org/2000/svg" width="25px" fill="currentColor" stroke="currentColor" viewBox="0 0 24 24"><title>menu</title><path d="M3,8V7H20V8H3M20,12V13H3V12H20M3,17H20V18H3V17Z" /></svg>';
-    let sidebarIcon = document.createElement('div');
+    sidebarIcon = document.createElement('div');
     sidebarIcon.setAttribute('class', 'sidebar-icon');
     sidebarIcon.innerHTML = sidebarIconCode;
+    sidebarIcon.addEventListener('click', expandSidebar);
     contentHeader.appendChild(sidebarIcon);
 };
 
 let expandSidebar = () => {
+    contentHeader.removeChild(sidebarIcon);
     sidebarContainer.style.width = '280px';
     nav.style.backgroundColor = 'var(--color-nav-bg)';
     body.style.gridTemplateColumns = '280px 1fr';
