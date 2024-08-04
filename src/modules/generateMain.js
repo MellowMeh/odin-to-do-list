b127d4833b85cfb9454e83d6c051fdb372f178cf
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-08-04 18:00:51 -0400

    Add customDate class

--- src/modules/generateMain.js
+++ src/modules/generateMain.js
@@ -8,10 +8,27 @@ let selectedObjectDueDate;
 let selectedObjectPriority;
 let selectedObjectProject;
 let selectedObjectId;
-let currentDate = new Date().toJSON().slice(0, 10);
-console.log(currentDate);
+class MyCustomDate extends Date {
+    addDays(days) {
+      const date = new MyCustomDate(this.valueOf());
+      date.setDate(date.getDate() + days);
+      return date;
+    }
+  }
+  const todayRaw = new MyCustomDate();
+     let today = JSON.stringify(todayRaw).slice(1, 11);
+  const tomorrowRaw = todayRaw.addDays(1);
+    let tomorrow = JSON.stringify(tomorrowRaw).slice(1, 11);
+  const twoDaysAwayRaw = todayRaw.addDays(2);
+    let twoDaysAway = JSON.stringify(twoDaysAwayRaw).slice(1, 11);
+  const threeDaysAwayRaw = todayRaw.addDays(3);
+    let threeDaysAway = JSON.stringify(threeDaysAwayRaw).slice(1, 11);
+  const fourDaysAwayRaw = todayRaw.addDays(4);
+    let fourDaysAway = JSON.stringify(fourDaysAwayRaw).slice(1, 11);
+  const fiveDaysAwayRaw = todayRaw.addDays(5);
+    let fiveDaysAway = JSON.stringify(fiveDaysAwayRaw).slice(1, 11);
 
-
+    
 let generateInbox = () => {
     removeInboxCards();
     contentHeader.textContent = 'inbox';
